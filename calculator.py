94328f197c9fc7effca664e0533486b54cc6148b
felipenasci <dfnascimento@live.com>
2026-02-21 16:54:59 -0300

    feat: add isentropic enthalpy and efficiency calculations

--- calculator.py
+++ calculator.py
@@ -30,3 +30,24 @@ def calculateMassFlow(temperatureInput, temperatureOutput, pressureInput, pressu
     mass = power / (boilerEfficiency * machineEfficiency * (enthalpyInput - enthalpyOutput))
 
     return mass / 10000000
+
+def calculateIsentropicEnthalpy(pressureInput, temperature, pressureOutput):
+    if pressureInput <= 0 or pressureOutput <= 0 or temperature <= 0:
+        return 0.0
+    
+    _pressureInput = convertToPA(pressureInput)
+    _temperature = convertToKelvin(temperature)
+    _pressureOutput = convertToPA(pressureOutput)
+
+    # Entropia constante
+    entropy = PropsSI('S', 'P', _pressureInput, 'T', _temperature, 'Water')
+    # Entalpia na saída com S constante
+    IsentropicEnthalpy = PropsSI('H', 'P', _pressureOutput, 'S', entropy, 'Water')
+    
+    return IsentropicEnthalpy / 1000
+
+def calculateEfficiency(enthalpyInput,  enthalpyOutput, isentropicEnthalpy):
+    if (enthalpyInput - isentropicEnthalpy <= 0):
+      return 0.0
+
+    return (enthalpyInput - enthalpyOutput) / (enthalpyInput - isentropicEnthalpy)
